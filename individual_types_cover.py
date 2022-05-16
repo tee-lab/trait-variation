4af311f89fc1d1408dc745445d7eacc4e3654a30
Tanveen <57477444+tanrancode@users.noreply.github.com>
2022-05-16 16:07:45 +0530

    Update individual_types_cover.py

--- individual_types_cover.py
+++ individual_types_cover.py
@@ -1,27 +1,38 @@
-
-
 import numpy as np
-import math
-import matplotlib.pyplot as plt
-import statistics as st
 import pandas as pd
-from scipy.stats import uniform
+import time
+from numba import njit
+from multiprocessing import Pool
+from scipy.stats import beta, uniform
 
+# If one wishes to run this file from the terminal, one can specify the varying trait 
+# and the distribution of the trait as arguments. For example: python filename -t u -d unif
+# Here -t refers to the trait and -d refers to the distribution of the trait
+import getopt
+import sys
+
+# Remove the first argument( the filename)
+all_args = sys.argv[1:]
+
+try:
+   # Gather the arguments
+   opts, args = getopt.getopt(all_args, 't:d:')
+except:
+        print("Error")
+        
+print("opts:", opts)
+print("args:", args) 
+   
+for opt, arg in opts:
+    if opt in ['-t']:
+        trait = str(arg)
+    elif opt in ['-d']:
+        dist = str(arg)
+
+
+# The following code defines a function crange, that generates a sequence 
+# while making sure that the start and end points are included in it
 def cust_range(*args, rtol=1e-05, atol=1e-08, include=[True, False]):
-    """
-    Combines numpy.arange and numpy.isclose to mimic
-    open, half-open and closed intervals.
-    Avoids also floating point rounding errors as with
-    >>> numpy.arange(1, 1.3, 0.1)
-    array([1. , 1.1, 1.2, 1.3])
-
-    args: [start, ]stop, [step, ]
-        as in numpy.arange
-    rtol, atol: floats
-        floating point tolerance as in numpy.isclose
-    include: boolean list-like, length 2
-        if start and end point are included
-    """
     # process arguments
     if len(args) == 1:
         start = 0
@@ -59,95 +70,166 @@ def orange(*args, **kwargs):
 
 print('crange(1, 1.3, 0.1) >>>', crange(1, 1.3, 0.1))
 
-def omega(G,th):  
-    return 0.9 + (0.05-0.9)/(1+math.exp((th-G)/0.005))
+# The following functions encode for the ODEs using Euler method for different
+# varying traits: sapling death rate (u), tree death rate (v) and sapling resistance to fire (th), respectively.
 
-Gseq = np.arange(0,1.05,0.1) 
-df_th = pd.DataFrame({'Gi':Gseq,'T1': [0.0]*len(Gseq), 'T2':[0.0]*len(Gseq),'T3':[0.0]*len(Gseq),'T4':[0.0]*len(Gseq),'T5':[0.0]*len(Gseq)})
-    #, 'T6': [0.0]*len(Gseq), 'T7':[0.0]*len(Gseq),'T8':[0.0]*len(Gseq),
-     #                 'T9':[0.0]*len(Gseq),'T10':[0.0]*len(Gseq)})
-
-for r in Gseq: 
-    bins = 5
-    min_var = 0.25
-    max_var = 0.75
-    #min_var = 0.35
-    #max_var = 0.65
-    th = list(np.arange(min_var, max_var,0.0001))
-    x = max_var - min_var
-    binsize = (x-0.1)/(bins-1)
-    a =  np.empty(bins)
-    l = uniform.pdf(th)
-    q= list(crange(0.3,0.7,binsize))
-    q  = [round(i,1) for i in q]
-    v = range(0,bins,1)
-    for i in v:
-        a[i]=st.mean(l[int(((i)*((len(th)-1)/bins))):int(((i+1)*((len(th)-1)/bins)+1))])
-        a[i] = (a[i]*uniform.pdf(q[i],min_var,max_var))
-    a
-    p = a/sum(a)
-    x = 10000
-    pr = p*x
-    a_beta1 = np.empty(0)
-    for i in range(0,len(pr)):
-        ab = np.array([q[i]]*int(pr[i]))
-        a_beta1 = np.append(a_beta1,ab)
-    d=len(a_beta1)
-    Gi = G0 = r
-    S0 = T0 = (1-G0)/2
-    b, u, v = 0.35, 0.1, 0.1
-    S_t = Si = [S0/d for i in range(0,d)]
-    T_t = Ti = [T0/d for i in range(0,d)]
-    G = [G0 for i in range(0,d)]
-    tim = 200
-    dt = 0.001
-    timesteps = int(tim/dt)
-    df = pd.DataFrame(a_beta1, columns=['th'])
-    df['FinS'] = Si
-    df['FinT'] = Ti
-    tic1 = time.process_time()
+@njit(fastmath=False)
+def props_u(g):
+    global d, timesteps, u, v, dt, th, b, p
+    G0 = g # Intial G value
+    S0 = T0 = (1-G0)/2  # Initial total S and T value
+    # Multiplying with p gives the proportion cover of each trait type
+    Si = S0*p
+    Ti = T0*p 
+  
     for t in range(0,timesteps):
-        G_t=G0+(u*S0 + v*T0 - b*G0*T0)*dt
-        for i in q:
-            z1=df[df['th']==i].index.tolist()
-            z1[0]
-            S_i = df.loc[z1[0],'FinS'] + (b*G0*df.loc[z1[0],'FinT'] - omega(G0,i)*df.loc[z1[0],'FinS'] - u*df.loc[z1[0],'FinS'])*dt;
-            T_i = df.loc[z1[0],'FinT']+ (omega(G0,i)*df.loc[z1[0],'FinS'] - v*df.loc[z1[0],'FinT'])*dt; 
+          death = u*Si
+          G_t= G0 + (np.sum(death) + v*np.sum(Ti) - G0*b*np.sum(Ti))*dt
+          for i in range(0,d):
+              s = Si[i]
+              Si[i] += (b*G0*Ti[i] - (0.9 + (0.05-0.9)/(1+np.exp((th-G0)/0.005)))*s - u[i]*s)*dt;
+              Ti[i] += ((0.9 + (0.05-0.9)/(1+np.exp((th-G0)/0.005)))*s - v*Ti[i])*dt; 
+          G0 = G_t
+          S0 = np.sum(Si)
+          T0 = np.sum(Ti)
+
+    return Si, Ti 
+
+
+@njit(fastmath=False)
+def props_v(g):
+    global d, timesteps, u, v, dt, th, b, p
+    G0 = g
+    S0 = T0 = (1-G0)/2
+    Si = S0*p
+    Ti = T0*p 
+          
+    for t in range(0,timesteps):
+          G_t= G0 + (u*np.sum(Si) + np.sum(v*Ti) - G0*b*np.sum(Ti))*dt
+          for i in range(0,d):
+              s = Si[i]
+              Si[i] += (b*G0*Ti[i] - (0.9 + (0.05-0.9)/(1+np.exp((th-G0)/0.005)))*s - u*s)*dt;
+              Ti[i] += ((0.9 + (0.05-0.9)/(1+np.exp((th-G0)/0.005)))*s - v[i]*Ti[i])*dt; 
+          G0 = G_t
+          S0 = np.sum(Si)
+          T0 = np.sum(Ti)
+
+    return Si, Ti 
+
+@njit(fastmath=False)
+def props_th(g):
+    global d, timesteps, u, v, dt, th, b, p
+    G0 = g
+    S0 = T0 = (1-G0)/2
+    Si = S0*p
+    Ti = T0*p 
+          
+    for t in range(0,timesteps):
+          G_t= G0 + (u*np.sum(Si) + v*np.sum(Ti) - G0*b*np.sum(Ti))*dt
+          for i in range(0,d):
+              s = Si[i]
+              Si[i] += (b*G0*Ti[i] - (0.9 + (0.05-0.9)/(1+np.exp((th[i]-G0)/0.005)))*s - u*s)*dt;
+              Ti[i] += ((0.9 + (0.05-0.9)/(1+np.exp((th[i]-G0)/0.005)))*s - v*Ti[i])*dt; 
+          G0 = G_t
+          S0 = np.sum(Si)
+          T0 = np.sum(Ti)
+
+    return Si, Ti 
+
+Gseq = np.arange(0,1,0.1) # Initial values of G
+Gseq = np.round(Gseq,2)
+
+df_S_Wo = pd.DataFrame() # Dataframe to store individual sapling cover of each trait type 
+df_T_Wo = pd.DataFrame() # Dataframe to store individual tree cover of each trait type 
+
+d = 10 # No. of trait types 
+# Range of trait values: (0,1)
+min_var = 0
+max_var = 1
+b = 0.45 # Setting sapling birth rate = 0.45 corresponds to the woodland regime
+print("b:",b)    
+diff = max_var - min_var
+binsize = (diff)/(d)
+tr = crange(min_var+(binsize/2),max_var-(binsize/2),binsize) # Set of trait values
+
+
+tim = 10000
+dt = 0.001 # size of each time step
+timesteps = int(tim/dt)
+
+# Set the Distribution for the trait with 'dist=', if not running from the terminal directly
+# p[i] tells the proportion of each trait value out of 1
+p =  np.empty(d)
+sump=0
+
+if dist=="bimod": # This refers to bimodal beta distribution
+    for i in tr:
+        sump += beta.pdf(i,2,8)/2 + beta.pdf(i,8,2)/2
+    for i in range(0,d,1):
+        p[i] = (beta.pdf(tr[i],2,8)/2 + beta.pdf(tr[i],8,2)/2)/sump  
+if dist=="beta": # This refers to unimodal beta distribution
+    for i in tr:
+        sump += beta.pdf(i,4,4)
+    for i in range(0,d,1):
+        p[i] = beta.pdf(tr[i],4,4)/sump
+if dist=="unif": # This refers to uniform distribution
+    for i in tr:
+        sump += uniform.pdf(i,min_var,max_var)
+    for i in range(0,d,1):
+        p[i] = uniform.pdf(tr[i],min_var,max_var)/sump
+
+
+# Set the varying trait with 'trait=', if not running from the terminal directly
+if trait=="v":
+    v = crange(min_var+(binsize/2),max_var-(binsize/2),binsize)
+    v = np.round(v,3)
+    u = 0.05
+    th = 0.5
+    
+    #This runs props_v() function for different values of G0 in parallel:
+    if __name__ == '__main__':
+        pool = Pool() 
+        start = time.time()
+        x = pool.map(props_v, Gseq)
+        print("Complete")
+        end = time.time()
+        print('total time (s)= ' + str(end-start))
         
-            df.loc[z1,'FinS']= S_i
-            df.loc[z1,'FinT'] = T_i
-            
-        G0 = G_t
-        S0 = sum(df.loc[:,'FinS'])
-        T0 = sum(df.loc[:,'FinT'])
-    toc1 = time.process_time()
-    print(toc1 - tic1)
-#    z = int(r*10)
-#    df_th.loc[z,'T1']= sum(df.loc[df['th']==q[0],'FinT'])
-#    df_th.loc[[z],['T2']]= sum(df.loc[df['th']==q[1],'FinT'])
-#    df_th.loc[[z],['T3']]= sum(df.loc[df['th']==q[2],'FinT'])
-#    df_th.loc[[z],['T4']]= sum(df.loc[df['th']==q[3],'FinT'])
-#    df_th.loc[[z],['T5']]= sum(df.loc[df['th']==q[4],'FinT'])
-#    df_th.loc[z,'T6']= sum(df.loc[df['th']==q[5],'FinT'])
-#    df_th.loc[[z],['T7']]= sum(df.loc[df['th']==q[6],'FinT'])
-#    df_th.loc[[z],['T8']]= sum(df.loc[df['th']==q[7],'FinT'])
-#    df_th.loc[[z],['T9']]= sum(df.loc[df['th']==q[8],'FinT'])
-#    df_th.loc[[z],['T10']]= sum(df.loc[df['th']==q[9],'FinT'])
-    z=df_th[df_th['Gi']==r].index.tolist()
-    df_th.loc[z[0],'T1']= sum(df.loc[df['th']==q[0],'FinT'])
-    df_th.loc[[z[0]],['T2']]= sum(df.loc[df['th']==q[1],'FinT'])
-    df_th.loc[[z[0]],['T3']]= sum(df.loc[df['th']==q[2],'FinT'])
-    df_th.loc[[z[0]],['T4']]= sum(df.loc[df['th']==q[3],'FinT'])
-    df_th.loc[[z[0]],['T5']]= sum(df.loc[df['th']==q[4],'FinT'])
-#    df_th.loc[z[0],'T6']= sum(df.loc[df['th']==q[5],'FinT'])
-#    df_th.loc[[z[0]],['T7']]= sum(df.loc[df['th']==q[6],'FinT'])
-#    df_th.loc[[z[0]],['T8']]= sum(df.loc[df['th']==q[7],'FinT'])
-#    df_th.loc[[z[0]],['T9']]= sum(df.loc[df['th']==q[8],'FinT'])
-#    df_th.loc[[z[0]],['T10']]= sum(df.loc[df['th']==q[9],'FinT'])
+if trait=="u":
+     u = crange(min_var+(binsize/2),max_var-(binsize/2),binsize)
+     u = np.round(u,3)
+     v = 0.1
+     th = 0.5
+     
+     #This runs props_u() function for different values of G0 in parallel:
+     if __name__ == '__main__':
+         pool = Pool() 
+         start = time.time()
+         x = pool.map(props_u,Gseq)
+         print("Complete")
+         end = time.time()
+         print('total time (s)= ' + str(end-start))
+         
+if trait=="th":
+     th = crange(min_var+(binsize/2),max_var-(binsize/2),binsize)
+     th = np.round(th,3)
+     u = 0.2
+     v = 0.1
+    
+     #This runs props_th() function for different values of G0 in parallel:
+     if __name__ == '__main__':
+         pool = Pool() 
+         start = time.time()
+         x = pool.map(props_th, Gseq)
+         print("Complete")
+         end = time.time()
+         print('total time (s)= ' + str(end-start))
 
-q2 = [round(x,2) for x in q if type(x)==np.float64]
-q2.insert(0,'Gi')
-df_th.columns = q2
+# Storing obtained individual sapling and tree covers to the dataframes
+for i in range(len(Gseq)):
+    df_S_Wo[i/10]=x[i][0] 
+    df_T_Wo[i/10]=x[i][1] 
 
-f, ax = plt.subplots()    
-df_th.plot(x="Gi", kind="bar", stacked=True, cmap="plasma")
+# Saving the dataframes to a CSV for further analysis:
+df_S_Wo.to_csv('S_Indi_prop_'+trait+'_'+dist+'.csv')
+df_T_Wo.to_csv('T_Indi_prop_'+trait+'_'+dist+'.csv')
